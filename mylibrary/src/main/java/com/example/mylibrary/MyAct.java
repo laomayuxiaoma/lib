9d646438d9013437ad4792744255f3e71da3d06f
zhangming <zhangming@163.com>
2018-06-28 09:08:00 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyAct.java
@@ -0,0 +1,17 @@
+package com.example.mylibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2018/6/27.
+ */
+
+public class MyAct extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
